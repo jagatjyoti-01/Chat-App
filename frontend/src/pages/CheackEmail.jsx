c82debb7eff7120a2026ca24120c1f348a1f8a2b
jagatjyoti-01 <jagatjyotidash33@gmail.com>
2025-08-03 22:18:39 +0530

    .

---
+++ frontend/src/pages/CheackEmail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CheackEmail() {
+  return (
+    <div>CheackEmail</div>
+  )
+}
+
+export default CheackEmail
---
+++ frontend/src/pages/CheackPassword.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CheackPassword() {
+  return (
+    <div>CheackPassword</div>
+  )
+}
+
+export default CheackPassword
