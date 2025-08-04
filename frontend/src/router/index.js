91559bfd8f1eb4a8fc7ec4f414ce7d07610c8cbe
jagatjyoti-01 <jagatjyotidash33@gmail.com>
2025-08-04 23:44:41 +0530

    .

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,42 @@
 import { createBrowserRouter } from "react-router-dom";
+import Register from "../pages/Register";
+import CheackEmail from "../pages/CheackEmail";
+import CheackPassword from "../pages/CheackPassword";
+import Home from "../pages/Home";
+import MessagePage from "../components/MessagePage";
+import App from "../App";
 
 
 const router=createBrowserRouter([
-    path
+    {
+    path : "/",
+    element : <App/>,
+    children : [
+        {
+            path : "register",
+            element : <Register/>
+        },
+        {
+            path:"email",
+            element:<CheackEmail/>
+        },
+        {
+            path:"password",
+            element:<CheackPassword/>
+        },
+        {
+            path:"",
+            element:<Home/>,
+            children:[
+                {
+                    path:'userId',
+                    element:<MessagePage/>
+                }
+            ]
+        }
+    ]
+}
+
 ])
+
+export default router
